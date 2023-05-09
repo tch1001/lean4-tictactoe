86eb4adf60176f835d263e4d47ff12429875f7df
tch1001 <tanchienhao@gmail.com>
2023-05-09 16:51:52 +0800

    update

--- Tictactoe.lean
+++ Tictactoe.lean
@@ -1,4 +1,5 @@
-    
+-- import Lean 
+-- open Lean Widget -- for the future
 
 structure Cell where
   isEmpty : Bool
@@ -8,7 +9,24 @@ deriving Repr
 def cell1 : Cell :=
   { isEmpty := true, value := 0 }
 
-def sudoku_board : Array Cell :=
-  #[cell1]
+#check cell1
+#print cell1
 
-#print sudoku_board
+structure SudokuState where
+  board : Array (Array Cell)
+deriving Repr
+
+def printRow : Option (Array Cell) → Option Cell :=
+  fun row => 
+    match row with
+    | none => none
+    | some row => row[0]?
+
+def printState : SudokuState → Option (Cell) := 
+  fun state => printRow (state.board[0]?)
+
+def sudokuState1 : SudokuState :=
+  { board := #[ #[ cell1, cell1, cell1 ], #[ cell1, cell1, cell1 ], #[ cell1, cell1, cell1 ] ] }
+
+#eval printState sudokuState1
+
